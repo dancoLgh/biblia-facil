80f00db5409eea66d95a6e3bd9e73ab35e92dc98
Daniel Resoalbe <33226168+dancoLgh@users.noreply.github.com>
2025-08-03 17:11:51 -0300

    init

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+js
+const CACHE_NAME = 'biblia-app-v2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/bible.xml',
+  '/service-worker.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  // Limpiar cachés antiguos
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+    ))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => caches.match('/index.html'))
+  );
+});
